36bcffa7e6aae65c85811d48dde20bddba076ffa
Justin Baum <justin.baum@ninefx.com>
2019-01-20 12:42:12 -0500

    Add pointers1

--- data_structures/pointers1.cc
+++ data_structures/pointers1.cc
@@ -17,6 +17,24 @@ char * returnchars() {
     return r;
 }
 
+int * returnpointer2() {
+    int r[100];
+    for(int i = 0; i < 100; i++) {
+        r[i] = i;
+        std::cout << i << std::endl;
+    }
+    return r;
+};
+
+char * returnchars2() {
+    char r[100];
+    for(char c = 0; c < 100; c++) {
+        r[c] = c;
+        std::cout << c << std::endl;
+    }
+    return r;
+}
+
 int main(int args, char *argv[]) {
     int *p;
     p = returnpointer();
@@ -27,6 +45,16 @@ int main(int args, char *argv[]) {
     c = returnchars();
     for(int i = 0; i < 100; i++) {
         std::cout << "*(c + " << i << ") = " << *(c+i) << std::endl;
+    };
+    int *pp;
+    pp = returnpointer2();
+    for(int i = 0; i < 100; i++) {
+        std::cout << "*(pp + " << i << ") = " << *(pp+i) << std::endl;
+    };
+    char *cc;
+    cc = returnchars2();
+    for(int i = 0; i < 100; i++) {
+        std::cout << "*(cc + " << i << ") = " << *(cc+i) << std::endl;
     };
     return 0;
 };
